586d08ee1f4b3b3acbf16107319807a297b7822c
Nicholas Benedict <nicholasdbenedict@gmail.com>
2021-11-17 09:05:37 -0500

    Add project files.

---
+++ RedBadgeFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedBadgeFinal.Startup))]
+namespace RedBadgeFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
